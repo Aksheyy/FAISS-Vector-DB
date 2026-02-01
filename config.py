597ac7b71cd1457b5fcd3dc99f5ed7bc724de0f2
Akshay Sreedhar <127430405+Aksheyy@users.noreply.github.com>
2026-02-01 23:36:19 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-AZURE_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
-AZURE_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
-AZURE_DEPLOYMENT = os.getenv("AZURE_OPENAI_DEPLOYMENT")
